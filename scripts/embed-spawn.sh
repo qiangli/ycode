084813eccf9af2d33ae0ebe30b6a014d89ff4f62
Qiang Li <liqiang@gmail.com>
2026-06-30 07:40:22 -0700

    fix: revert gzip absolute path workaround since bashy now supports -n

--- scripts/embed-spawn.sh
+++ scripts/embed-spawn.sh
@@ -21,9 +21,7 @@ TMP="$(mktemp -d)"
 trap 'rm -rf "${TMP}"' EXIT
 
 (cd "${REPO_ROOT}" && go build -trimpath -ldflags="-s -w" -o "${TMP}/ycode-spawn" ./cmd/ycode-spawn/)
-GZIP_BIN="/usr/bin/gzip"
-[ ! -x "$GZIP_BIN" ] && GZIP_BIN="/bin/gzip"
-"$GZIP_BIN" -n -9 -c "${TMP}/ycode-spawn" > "${TMP}/ycode-spawn.gz"
+gzip -n -9 -c "${TMP}/ycode-spawn" > "${TMP}/ycode-spawn.gz"
 
 if cmp -s "${TMP}/ycode-spawn.gz" "${OUT_GZ}" 2>/dev/null; then
     exit 0
